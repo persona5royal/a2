b8ce533ed2997c98043f72f25aef27dfcd1754eb
hopebeyond <49632356+persona5royal@users.noreply.github.com>
2023-02-12 12:22:47 -0800

    secondary commit - moving the template files in

---
+++ venv/a2.py
@@ -0,0 +1,9 @@
+# a2.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME
+# EMAIL
+# STUDENT ID
---
+++ venv/ui.py
@@ -0,0 +1,9 @@
+# ui.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME
+# EMAIL
+# STUDENT ID
